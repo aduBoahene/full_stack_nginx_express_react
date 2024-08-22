e1626246dcd6a2d226a262be738622e38eeb1c1a
aduBoahene <boahenepatrick2@gmail.com>
2024-08-22 06:36:33 +0000

    removed excess files from fr app

--- reverse-proxy-fr/types/types.ts
+++
@@ -1,9 +0,0 @@
-// types.ts
-
-export interface ExtendedDocument {
-    // Define the properties of ExtendedDocument here
-    title: string;
-    content: string;
-    // Add other properties as needed
-  }
-  
